74bb69a7b0a76a7bb4d0fe05cabffa61ea7b4192
clicket7 <clicket7@hotmail.com>
2017-07-11 09:39:44 +0300

    Realized simple ChatServer. It works, but need to improve in future.

--- src/chatServer/ChatServer.java
+++ src/chatServer/ChatServer.java
@@ -1,9 +1,85 @@
 package chatServer;
 
-public class ChatServer {
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
 
-	public ChatServer() {
-		// TODO Auto-generated constructor stub
+public class ChatServer implements Runnable {
+
+	public static int port = 9999;
+	static ServerSocket server;
+	static Vector<ChatServer> connections;
+
+	private Socket client;
+	private Scanner in;
+	private PrintWriter out;
+	private String name;
+	
+	public static void main(String[] args) {
+		connections = new Vector<ChatServer>();
+		
+		try {
+			server = new ServerSocket(port);
+		} catch (IOException e) {
+			System.out.println("Can't create ServerSocket for Server!");
+			System.exit(1);
+		}
+
+		Socket socket = null;
+		Thread t = null;
+		
+		while (true) {
+			try {
+				socket = server.accept();
+				t = new Thread(new ChatServer(socket));
+				t.start();
+			} 
+			catch (IOException e) {
+				System.out.println("Can't create Socket for User!");
+			}
+		}
+	}
+	
+	public ChatServer(Socket client) {
+		this.client = client;
+		name = "Server";
+		connections.addElement(this);
+		
+		try {
+			in = new Scanner(new InputStreamReader(client.getInputStream()));
+			out = new PrintWriter(client.getOutputStream(), true);
+		} catch (IOException e) {
+			System.out.println("Can't initialize Input/Output for Client!");
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			String msg;
+			while (in.hasNext()) {
+				msg = in.nextLine().toString();
+				for (ChatServer server : connections) server.sendMsg(msg);
+				if (msg.equals("quit") || msg.equals("exit")) break;
+			}
+			break;
+		}
+		
+		try {
+			in.close();
+			out.close();
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMsg(String msg) {
+		out.println(name + ": " + msg);
 	}
 
 }
--- src/chatServer/ChatWindow.java
+++
@@ -1,5 +0,0 @@
-package chatServer;
-
-public class ChatWindow {
-
-}
